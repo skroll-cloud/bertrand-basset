eb95adc8ec18ed6f75a89ae99fe7061df8498637
skroll-cloud <bertrand.basset@gmail.com>
2026-03-09 14:23:08 +0100

    Admin: update — 09/03/2026 14:23

---
+++ js/gallery-images.js
@@ -0,0 +1,29 @@
+/**
+ * gallery-images.js
+ * Généré par Admin V4 — 09/03/2026 14:23:07
+ * Ne pas éditer manuellement.
+ */
+const GALLERY_IMAGES = {
+    "portrait": [
+        { "filename": "02.jpg" },
+        { "filename": "conversation-02.jpg" },
+        { "filename": "Imane02@bertrandbasset 2.jpg" },
+        { "filename": "JF.jpg" },
+        { "filename": "L1020630.jpg" },
+        { "filename": "placeholder-8.jpg" },
+        { "filename": "portrait-01.jpg" },
+        { "filename": "portrait-02.jpg" }
+    ],
+    "conversation-s-": [
+        { "filename": "conversation-01.jpg" },
+        { "filename": "conversation-02.jpg" }
+    ],
+    "studio": [
+        { "filename": "stmelar-1.jpg" },
+        { "filename": "stmelar-2.jpg" },
+        { "filename": "stmelar-3.jpg" },
+        { "filename": "stmelar-4.jpg" },
+        { "filename": "stmelar-5.jpg" },
+        { "filename": "stmelar-6.jpg" }
+    ],
+};
