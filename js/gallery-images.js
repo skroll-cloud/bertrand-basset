97a59a82fb79e4578653f465461f612bcda62c47
skroll-cloud <bertrand.basset@gmail.com>
2026-04-22 11:11:01 +0200

    Admin: update — 22/04/2026 11:10

--- js/gallery-images.js
+++ js/gallery-images.js
@@ -1,6 +1,6 @@
 /**
  * gallery-images.js
- * Généré par Admin V4 — 22/04/2026 11:09:24
+ * Généré par Admin V4 — 22/04/2026 11:10:49
  * Ne pas éditer manuellement.
  */
 const GALLERY_IMAGES = {
@@ -14,8 +14,7 @@ const GALLERY_IMAGES = {
         { "filename": "portrait-01.jpg" },
         { "filename": "portrait-02.jpg" },
         { "filename": "L1060508.jpg" },
-        { "filename": "Atelier_Plougasnou.jpg" },
-        { "filename": "Atelier_Plougasnou 1.jpg" }
+        { "filename": "Atelier_Plougasnou.jpg" }
     ],
     "conversation-s-": [
         { "filename": "conversation-01.jpg" },
